f223aca0b36a16a5e5eb9eb31e17e1ad363cd6f7
Phuong <phuongminh2303@gmail.com>
2019-04-22 12:13:25 +0700

    Refractor UI and pizza image

--- Project/seed/seed.js
+++ Project/seed/seed.js
@@ -7,57 +7,57 @@ var foodData = [
         name: "Cheese Pizza",
         type: "pizza",
         cost: 10.05,
-        image: "https://images.pexels.com/photos/2232/vegetables-italian-pizza-restaurant.jpg?auto=compress",
+        image: "https://images.pexels.com/photos/2232/vegetables-italian-pizza-restaurant.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         description: "Real cheese made from mozzarella, pizza sauce and buttery garlic crust."
     },
     {
         name: "Pepperoni Pizza",
         type: "pizza",
         cost: 10.95,
-        image: "https://images.pexels.com/photos/774487/pexels-photo-774487.jpeg?auto=compress",
+        image: "https://images.pexels.com/photos/774487/pexels-photo-774487.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         description: "Premium pepperoni, real cheese made from mozzarella and buttery garlic crust."
     },
     {
         name: "Sausage Pizza",
         type: "pizza",
         cost: 11.25,
-        image: "https://images.pexels.com/photos/1552635/pexels-photo-1552635.jpeg?auto=compress",
+        image: "https://images.pexels.com/photos/1552635/pexels-photo-1552635.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         description: "Sausage and real cheese made from mozzarella and buttery garlic crust."
     },
     {
         name: "Garden Pizza",
         type: "pizza",
         cost: 11.15,
-        image: "https://images.pexels.com/photos/263041/pexels-photo-263041.jpeg?auto=compress",
+        image: "https://images.pexels.com/photos/263041/pexels-photo-263041.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         description: "Onions, sliced tomatoes, pizza sauce and signature three-cheese blend."
     },
     {
-        name: "Greek Pizza",
+        name: "Spicy Pizza",
         type: "pizza",
-        cost: 10.20,
-        image: "https://images.pexels.com/photos/1435907/pexels-photo-1435907.jpeg?auto=compress",
-        description: "Onions, sliced tomatoes, pizza sauce and signature three-cheese blend."
+        cost: 10.35,
+        image: "https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        description: "Spicy is not enough??? Try this! Then you will be surprised by its taste!!!"
     },
     {
-        name: "Greek Pizza",
+        name: "Homemade Pizza",
         type: "pizza",
         cost: 10.20,
-        image: "https://images.pexels.com/photos/1435907/pexels-photo-1435907.jpeg?auto=compress",
-        description: "Onions, sliced tomatoes, pizza sauce and signature three-cheese blend."
+        image: "https://images.pexels.com/photos/905847/pexels-photo-905847.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        description: "Best pizza for a cozy holiday. Remind you of the pizza in your childhood!"
     },
     {
-        name: "Greek Pizza",
+        name: "Jahu Pizza",
         type: "pizza",
         cost: 10.20,
-        image: "https://images.pexels.com/photos/1435907/pexels-photo-1435907.jpeg?auto=compress",
-        description: "Onions, sliced tomatoes, pizza sauce and signature three-cheese blend."
+        image: "https://images.pexels.com/photos/1146760/pexels-photo-1146760.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        description: "From Jahu and nothing to complain. It's perfection of pizza!!!"
     },
     {
-        name: "Greek Pizza",
+        name: "Red pepper Pizza",
         type: "pizza",
         cost: 10.20,
-        image: "https://images.pexels.com/photos/1435907/pexels-photo-1435907.jpeg?auto=compress",
-        description: "Onions, sliced tomatoes, pizza sauce and signature three-cheese blend."
+        image: "https://images.pexels.com/photos/1049620/pexels-photo-1049620.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        description: "Red pepper, sliced tomatoes and pizza sauce combine together. That's crazy!"
     },
 
     // DRINKS
@@ -72,7 +72,7 @@ var foodData = [
         name: "Pepsi",
         type: "drink",
         cost: 120,
-        image: "https://images.pexels.com/photos/1292294/pexels-photo-1292294.jpeg?auto=compress",
+        image: "https://images.pexels.com/photos/1292294/pexels-photo-1292294.jpeg?auto=compress&fit=crop&w=500&q=60g",
         description: "200 calories"
     },
     {
